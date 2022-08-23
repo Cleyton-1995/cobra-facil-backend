7897614119621697b1afd7c9d71118f09a42582a
Cleyton-1995 <cleytonotyelc1995@gmail.com>
2022-08-23 20:44:41 -0300

    creating models with prism

---
+++ prisma/migrations/20220823231522_create_table_charge/migration.sql
@@ -0,0 +1,30 @@
+/*
+  Warnings:
+
+  - You are about to drop the `voucher` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE `voucher` DROP FOREIGN KEY `voucher_userId_fkey`;
+
+-- DropTable
+DROP TABLE `voucher`;
+
+-- CreateTable
+CREATE TABLE `charge` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `userId` INTEGER NOT NULL,
+    `name` VARCHAR(75) NOT NULL,
+    `description` LONGTEXT NOT NULL,
+    `status` TINYINT NOT NULL,
+    `value` DECIMAL(7, 2) NOT NULL,
+    `code` INTEGER NOT NULL,
+    `dueDate` DATETIME NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `charge` ADD CONSTRAINT `charge_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `user`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,6 @@
-import express from 'express';
-import routes from './routes';
+import "reflect-metadata";
+import express from "express";
 
 const app = express();
 
-app.use(express.json());
-
-app.use(routes);
-
-app.listen(3333, () => {
-  console.log('🚀 Server started on port 3333!')
-});
+app.listen(3000, () => console.log("Server is running"))
